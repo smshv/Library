b237ddc932b1e5c277dca538759de293320e69fa
shohaibMahmud <shohaib.mahmud@gmail.com>
2023-07-29 15:01:02 -0400

    Add buttons stylings

--- scripts/library.js
+++ scripts/library.js
@@ -1,4 +1,6 @@
 let library = [];
+let IS_LIB_EMPTY = true;
+const bookListTable = document.querySelector("#book-lists");
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -15,40 +17,52 @@ function addToLibrary(bookObject){
     library.push(bookObject);
 }
 
-function displayBook(bookObject, bookIndex, bookListTable){
-    const tableRow = document.createElement("tr");
-    tableRow.setAttribute("id", bookIndex.toString());
-    const td1 = document.createElement("td");
-    td1.textContent = bookObject.title;
-    tableRow.appendChild(td1);
-    const td2 = document.createElement("td");
-    td2.textContent = bookObject.author;
-    tableRow.appendChild(td2);
-    const td3 = document.createElement("td");
-    td3.textContent = bookObject.pages;
-    tableRow.appendChild(td3);
-    const td4 = document.createElement("td");
-    if (bookObject.read){
-        td4.textContent = "Read";
-    }else{
-        td4.textContent = "Not read yet";
+function addElement(parrentElem, elementName, textContent, classLabel){
+    const elem = document.createElement(elementName);
+    elem.textContent = textContent;
+    elem.setAttribute("class", classLabel);
+    parrentElem.appendChild(elem);
+    return elem;
+}
+
+function displayBook(bookObject, bookIndex){
+    const tableRow = bookListTable.insertRow(bookIndex+1);
+    const values = Object.values(bookObject);
+    for (let i=0; i<3; i++){
+        tableRow.insertCell(i).textContent = values[i] ;
     }
-    tableRow.appendChild(td4);
-    bookListTable.appendChild(tableRow);
+    const readCell = tableRow.insertCell(3);
+    let text = "Not read yet";
+    let readBttnClr = "#10b981";
+    if ( values[3] ){
+        text = "Read" ;
+        readBttnClr = "#db2777";
+    }
+    readCell.textContent = text;
+    const elem = addElement(readCell, "button", "Change status", "read-button");
+    elem.style["background-color"] = readBttnClr;
+    elem.setAttribute("data", bookIndex.toString());
+    addElement(tableRow.insertCell(4), "button", "Delete", "delete-button").setAttribute("data", bookIndex.toString())
 }
 
 function displayLibrary(bookListTable){
     console.log(bookListTable);
     for ( let i = 0; i < library.length; i++){
-        displayBook(library[i], i, bookListTable);
+        displayBook(library[i], i);
     }
+}
+
+function changeReadStatus(e){
+    const bookIndex = Number(e.currentTarget.getAttribute("data"));
+    library[bookIndex].read = (!library[bookIndex].read);
+    bookListTable.deleteRow(bookIndex+1);
+    displayBook(library[bookIndex], bookIndex);
 }
 
 function toggleDisplay(toHiddenElement, toVisibleElement){
     toHiddenElement.style["visibility"] = "hidden";
     toVisibleElement.style["visibility"] = "visible";
 }
-const bookListTable = document.querySelector("#book-lists");
 
 document.querySelector("#add-button").
 addEventListener("click", function(event) { toggleDisplay(event.currentTarget, 
@@ -60,15 +74,22 @@ addEventListener("click", ()=>{
     form.reset();
     toggleDisplay(form, document.querySelector("#add-button"))} );
 
- 
 document.querySelector("#book-entry").addEventListener(
     "submit", function (event) {
         event.preventDefault();
         const data = new FormData(this);
         const bookIndex = library.length;
         addToLibrary(new Book(data.get("title"), data.get("author"), data.get("pages"), data.has("read")));
-        displayBook(library[bookIndex], bookIndex, bookListTable);
+        displayBook(library[bookIndex], bookIndex);
         this.reset();
         toggleDisplay(this, document.querySelector("#add-button"));
+
+        if ( IS_LIB_EMPTY ){
+            document.querySelectorAll(".read-button").forEach(elem=>{
+                elem.addEventListener("click", changeReadStatus);
+            });
+            IS_LIB_EMPTY = false;
+        }
+
     }
 );
